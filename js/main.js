97c4dfd99d77a504d2ffa934caab6fdfa5601320
irajmrperfect <i.rajmrperfect@gmail.com>
2018-04-10 20:10:10 +0530

    background images changed and removed unneccary images

--- js/main.js
+++ js/main.js
@@ -47,45 +47,45 @@
 	
 	var offcanvasMenu = function() {
 
-		$('#page').prepend('<div id="gtco-offcanvas" />');
-		$('#page').prepend('<a href="#" class="js-gtco-nav-toggle gtco-nav-toggle gtco-nav-dark"><i></i></a>');
-		var clone1 = $('.menu-1 > ul').clone();
-		$('#gtco-offcanvas').append(clone1);
-		var clone2 = $('.menu-2 > ul').clone();
-		$('#gtco-offcanvas').append(clone2);
+		// $('#page').prepend('<div id="gtco-offcanvas" />');
+		// $('#page').prepend('<a href="#" class="js-gtco-nav-toggle gtco-nav-toggle gtco-nav-dark"><i></i></a>');
+		// var clone1 = $('.menu-1 > ul').clone();
+		// $('#gtco-offcanvas').append(clone1);
+		// var clone2 = $('.menu-2 > ul').clone();
+		// $('#gtco-offcanvas').append(clone2);
 
-		$('#gtco-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
-		$('#gtco-offcanvas')
-			.find('li')
-			.removeClass('has-dropdown');
+		// $('#gtco-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
+		// $('#gtco-offcanvas')
+		// 	.find('li')
+		// 	.removeClass('has-dropdown');
 
-		// Hover dropdown menu on mobile
-		$('.offcanvas-has-dropdown').mouseenter(function(){
-			var $this = $(this);
+		// // Hover dropdown menu on mobile
+		// $('.offcanvas-has-dropdown').mouseenter(function(){
+		// 	var $this = $(this);
 
-			$this
-				.addClass('active')
-				.find('ul')
-				.slideDown(500, 'easeOutExpo');				
-		}).mouseleave(function(){
+		// 	$this
+		// 		.addClass('active')
+		// 		.find('ul')
+		// 		.slideDown(500, 'easeOutExpo');				
+		// }).mouseleave(function(){
 
-			var $this = $(this);
-			$this
-				.removeClass('active')
-				.find('ul')
-				.slideUp(500, 'easeOutExpo');				
-		});
+		// 	var $this = $(this);
+		// 	$this
+		// 		.removeClass('active')
+		// 		.find('ul')
+		// 		.slideUp(500, 'easeOutExpo');				
+		// });
 
 
-		$(window).resize(function(){
+		// $(window).resize(function(){
 
-			if ( $('body').hasClass('offcanvas') ) {
+		// 	if ( $('body').hasClass('offcanvas') ) {
 
-    			$('body').removeClass('offcanvas');
-    			$('.js-gtco-nav-toggle').removeClass('active');
+  //   			$('body').removeClass('offcanvas');
+  //   			$('.js-gtco-nav-toggle').removeClass('active');
 				
-	    	}
-		});
+	 //    	}
+		// });
 	};
 
 
